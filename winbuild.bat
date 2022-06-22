ad175664fc3196c8baa796ecd6332bbc42c5b080
James Titcumb <james@asgrim.com>
2022-06-22 14:32:18 +0100

    Replace the CC sed in Makefile with nmake CC parameter

--- winbuild.bat
+++ winbuild.bat
@@ -2,6 +2,5 @@ rmdir /S /Q x64
 call phpize
 call configure --enable-scoutapm --enable-debug --with-php-build="C:\php-sdk\phpdev\vs16\x64\deps" --with-prefix="C:\php\"
 call sed -i 's/-d extension=/-d zend_extension=/g' Makefile
-call sed -i 's/CC="$(PHP_CL)"/CC="cl.exe"/g' Makefile
-call nmake
+call nmake CC=cl.exe
 call nmake run ARGS="-m"
